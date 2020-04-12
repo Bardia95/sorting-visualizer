eff115fea702087127e5ad17fb703b6d204434df
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-12 14:08:27 -0400

    Reverted to non-animated sorting visualizers

--- src/sorting_visualizer/core.cljs
+++ src/sorting_visualizer/core.cljs
@@ -1,28 +1,30 @@
 (ns sorting-visualizer.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [clojure.string :as str]
-            [sorting-visualizer.algorithms :refer [get-merge-sort-animations]]))
+            [clojure.string :as str]))
 
 ;; Constants
 ;; =========
-(def animation-speed-ms 1000)
-(def n-vector-bars 210)
-(def primary-color "turquoise")
-(def secondary-color "red")
+(def animation-speed-ms 10)
+(def n-vector-bars 50)
+(def sorted-color "green")
+(def unsorted-color "red")
+(def normal-color "blue")
+
+(defn height [v i] (str (* (v i)) "px"))
 
 (defn new-vector []
-  (vec (repeatedly n-vector-bars #(+ 5 (rand-int 725)))))
+  (vec (repeatedly n-vector-bars #(+ 5 (rand-int 500)))))
 
 ;; State
 ;; =====
-(def vect (r/atom (new-vector)))
+(def v (r/atom (new-vector)))
 
 ;; State changers
-;; =======
+;; ==============
 (defn reset-vector! []
-  (let [new-vect (new-vector)]
-    (reset! vect new-vect)))
+  (let [nv (new-vector)]
+    (reset! v nv)))
 
 (defn change-bg-color! [el color]
   (set! (.-backgroundColor (.-style el)) color))
@@ -30,55 +32,94 @@
 (defn change-height! [el n]
   (set! (.-height (.-style el)) (str n "px")))
 
-;; Animations
+
+;; Helpers
+;; =======
+(defn get-vector-bar [i]
+  (first (array-seq (js/document.getElementsByClassName "vector-bar") i)))
+
+
+;; Sorting algorithms
 ;; =========
-(defn merge-sort! [v]
-  (loop [animations (get-merge-sort-animations v)
-         i 0]
-    (if (< i (count animations))
-      (let [bars (array-seq (.getElementsByClassName js/document "vector-bar"))
-            color (if (= (mod i 3) 0) secondary-color primary-color)]
-        (if (not= 2 (mod i 3))
-          (let [[bar-one-idx bar-two-idx] (animations i)
-                bar-one (bars bar-one-idx)
-                bar-two (bars bar-two-idx)]
-            (js/setTimeout
-             #(do (change-bg-color! bar-one color)
-                  (change-bg-color! bar-two color))
-             (* i animation-speed-ms))
-            (recur animations (inc i)))
-          (let [[bar-one-idx new-height] (animations i)
-                [bar-one] (bars bar-one-idx)]
-            (js/setTimeout
-             #(do (change-bg-color! bar-one color)
-                  (change-height! bar-one new-height))
-             (* i animation-speed-ms))
-            (recur animations (inc i)))))
-      (do (js/console.log "Finished sorting")
-          (println "Finished sorting")))))
+(defn do-merge [l r]
+  (loop [v []
+         i  0
+         j  0]
+    (if (and (< i (count l)) (< j (count r)))
+      (if (< (l i) (r j))
+        (recur (conj v (l i)) (inc i) j)
+        (recur (conj v (r j)) i (inc j)))
+      (if (< i (count l))
+        (recur (conj v (l i)) (inc i) j)
+        (if (< j (count r))
+          (recur (conj v (r j)) i (inc j))
+          v)))))
+
+(defn merge-sort [v]
+  (if (< (count v) 2)
+    v
+    (let [mid   (quot (count v) 2)
+          split (split-at mid v)
+          left  (vec (merge-sort (first split)))
+          right (vec (merge-sort (peek split)))]
+      (do-merge left right))))
+
+
+(defn bubble [ys x]
+  (if-let [y (peek ys)]
+    (if (> y x)
+      (conj (pop ys) x y)
+      (conj ys x))
+    [x]))
+
+(defn bubble-sort [xs]
+  (let [ys (reduce bubble [] xs)]
+    (if (= xs ys)
+      xs
+      (recur ys))))
+
+
+(defn excl [v x]
+  (let [[a b] (split-with #(not= % x) v)]
+    (concat a (rest b))))
+
+(defn selection-sort [v]
+  (loop [a []
+         b v]
+    (if (empty? b)
+      a
+      (let [m (apply min b)]
+        (recur (conj a m) (excl b m))))))
+
+
+
 
 ;; Components
 ;; ==========
 (defn vector-bars [v]
-[:div.vector-container (for [i (range (count v))]
-                         ^{:key i}[:div.vector-bar
-                                   {:style {:background-color "blue"
-                                            :height (str (* (v i)) "px")}}])])
+  [:div.vector-container (for [i (range (count v))]
+                           ^{:key i}[:div.vector-bar
+                                     {:style {:background-color normal-color
+                                              :height (height v i)}}])])
 
 (defn app []
   [:div
-   [vector-bars @vect]
+   [vector-bars @v]
    [:input {:type "button" :value "New vector"
             :on-click #(reset-vector!)}]
    [:input {:type "button" :value "Merge sort"
-            :on-click #(merge-sort! @vect)}]])
+            :on-click #(swap! v merge-sort)}]
+   [:input {:type "button" :value "Bubble sort"
+            :on-click #(swap! v bubble-sort)}]
+   [:input {:type "button" :value "Selection sort"
+            :on-click #(swap! v selection-sort)}]])
 
 
 ;; Start
 ;; =====
 (defn ^:dev/after-load start []
-(rdom/render [app] (js/document.getElementById "app")))
+  (rdom/render [app] (js/document.getElementById "app")))
 
 (defn init []
-(js/console.log "init")
-(start))
+  (js/console.log "init")
+  (start))
